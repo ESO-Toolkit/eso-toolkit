8829d9453789794508222ff82b0abcfa4e105215
bkrupa <bkrupa21@gmail.com>
2025-08-25 03:48:07 -0400

    Caching fixes (#25)

--- src/store/events_data/buffEventsSlice.ts
+++ src/store/events_data/buffEventsSlice.ts
@@ -84,7 +84,7 @@ export const fetchBuffEvents = createAsyncThunk<
         state.cacheMetadata.lastFetchedTimestamp &&
         Date.now() - state.cacheMetadata.lastFetchedTimestamp < DATA_FETCH_CACHE_TIMEOUT;
 
-      if (isCached && isFresh && state.events.length > 0) {
+      if (isCached && isFresh) {
         return false; // Prevent thunk execution
       }
 
