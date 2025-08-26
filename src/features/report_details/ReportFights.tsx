455d98bab27f02179c9c314e65293c7c946df8c4
bkrupa <bkrupa21@gmail.com>
2025-08-26 00:56:46 -0400

    Fixed reports not loading correctly (#34)

--- src/features/report_details/ReportFights.tsx
+++ src/features/report_details/ReportFights.tsx
@@ -9,38 +9,15 @@ import { ReportFightsView } from './ReportFightsView';
 export const ReportFights: React.FC = () => {
   // Get current selected report and fight from context
   const { reportId, fightId } = useSelectedReportAndFight();
-<<<<<<< HEAD
-  const dispatch = useAppDispatch();
-  const client = useEsoLogsClientInstance();
-
-  const fights = useSelector((state: RootState) => state.report.data?.fights);
-  const loading = useSelector((state: RootState) => state.report.loading);
-  const error = useSelector((state: RootState) => state.report.error);
-  const currentReportId = useSelector((state: RootState) => state.report.reportId);
-  const reportStartTime = useSelector((state: RootState) => state.report.data?.startTime ?? null);
-
-  React.useEffect(() => {
-    if (reportId && client) {
-      // Clear existing data when fetching a new report
-      if (currentReportId !== reportId) {
-        dispatch(clearAllEvents());
-        dispatch(clearMasterData());
-      }
-      // The thunk now handles checking if data needs to be fetched internally
-      dispatch(fetchReportData({ reportId, client }));
-    }
-  }, [reportId, client, currentReportId, dispatch]);
-=======
   const { reportData, isReportLoading } = useReportData();
->>>>>>> f658e58 (Fixed reports not loading correctly (#34))
 
   return (
     <ReportFightsView
-      fights={reportData?.fights?.filter(Boolean) as FightFragment[]}
+      fights={(reportData?.fights?.filter(Boolean) as FightFragment[]) || []}
       loading={isReportLoading}
       fightId={fightId || undefined}
       reportId={reportId || undefined}
-      reportStartTime={reportStartTime}
+      reportStartTime={reportData?.startTime ?? null}
     />
   );
 };
