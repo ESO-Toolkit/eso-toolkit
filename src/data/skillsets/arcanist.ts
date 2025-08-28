87c7ae27d158d094ca76c02994b51955281f189a
braydenpb72 <braydenpb72@gmail.com>
2025-08-27 23:57:57 -0500

    added encratis behemoth

--- src/data/skillsets/arcanist.ts
+++ src/data/skillsets/arcanist.ts
@@ -604,3 +604,4 @@ export const arcanistData: SkillsetData = {
     },
   },
 };
+
--- src/utils/gearUtilities.ts
+++ src/utils/gearUtilities.ts
@@ -171,6 +171,7 @@ export const MONSTER_ONE_PIECE_HINTS = Object.freeze(
       'Chokethorn',
       'Domihaus',
       'Earthgore',
+      ' Encratis’s Behemoth'
       'Engine Guardian',
       'Euphotic Gatekeeper',
       'Galenwe’s Lament',
