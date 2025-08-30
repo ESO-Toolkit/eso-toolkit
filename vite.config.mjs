cfd96da01b8d24dc8b2ec996d1d0f612a738ca25
bkrupa <bkrupa21@gmail.com>
2025-08-30 02:36:53 -0400

    Another actions fix (#86)

--- vite.config.mjs
+++ vite.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
+    base: process.env.VITE_BASE_URL,
     plugins: [
       svgr({
         svgrOptions: {
