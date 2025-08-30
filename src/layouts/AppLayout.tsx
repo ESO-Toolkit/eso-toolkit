3cfb9e38a36e50a26916c34f2bc6fb4e419d2817
braydenpb72 <braydenpb72@gmail.com>
2025-08-29 20:11:05 -0500

    mobile improvments

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -16,8 +16,13 @@ export const AppLayout: React.FC = () => {
     return (
       <Box sx={{ position: 'relative', minHeight: '100vh', bgcolor: 'background.default' }}>
         <HeaderBar />
-        <Container maxWidth="md">
-          <Box sx={{ pt: 10, pb: 4, minHeight: '100vh', overflowY: 'auto' }}>
+        <Container 
+          maxWidth="md"
+          sx={{
+            px: { xs: 0, sm: 2 }
+          }}
+        >
+          <Box sx={{ pt: { xs: 0, sm: 2 }, pb: { xs: 0, sm: 4 }, minHeight: '100vh', overflowY: 'auto' }}>
             <Login />
           </Box>
         </Container>
@@ -30,8 +35,13 @@ export const AppLayout: React.FC = () => {
       <ReportFightProvider>
         <Box sx={{ position: 'relative', minHeight: '100vh', bgcolor: 'background.default' }}>
           <HeaderBar />
-          <Container maxWidth="md">
-            <Box sx={{ pt: 10, pb: 4, minHeight: '100vh', overflowY: 'auto' }}>
+          <Container 
+            maxWidth="md"
+            sx={{
+              px: { xs: 0, sm: 2 }
+            }}
+          >
+            <Box sx={{ pt: { xs: 0, sm: 8}, pb: { xs: 0, sm: 4 }, minHeight: '100vh', overflowY: 'auto' }}>
               <Outlet />
             </Box>
           </Container>
