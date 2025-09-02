9ae57a051ee8c98c28e53649f137d4e9194fe5d7
Bradlee Krupa <bkrupa21@gmail.com>
2025-09-02 02:26:33 -0400

    Updated page title to reflect the selected report

--- src/Constants.ts
+++ src/Constants.ts
@@ -2,3 +2,5 @@ export const DATA_FETCH_CACHE_TIMEOUT = 30 * 60 * 1000; // 30 minutes
 
 export const SENTRY_DSN =
   'https://2fecc82b1e85c33a943a5b69e0a9feba@o4509922239315968.ingest.us.sentry.io/4509922240364544';
+
+export const APPLICATION_NAME = 'ESO Log Insights by NotaGuild';
