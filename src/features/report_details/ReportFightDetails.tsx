55dee03de3b44c6849031a6ad0f77e9487e43526
bkrupa <bkrupa21@gmail.com>
2025-09-02 02:39:50 -0400

    Updated page title to reflect the selected report (#152)

--- src/features/report_details/ReportFightDetails.tsx
+++ src/features/report_details/ReportFightDetails.tsx
@@ -5,6 +5,8 @@ import { useSelectedReportAndFight } from '../../ReportFightContext';
 
 import { ReportFightDetailsView } from './ReportFightDetailsView';
 
+import { APPLICATION_NAME } from '@/Constants';
+
 export const ReportFightDetails: React.FC = () => {
   // Get current selected report and fight from context
   const { reportId, fightId } = useSelectedReportAndFight();
@@ -16,6 +18,14 @@ export const ReportFightDetails: React.FC = () => {
   const fight = React.useMemo(() => {
     return reportData?.fights?.find((f) => String(f?.id) === String(fightId));
   }, [reportData?.fights, fightId]);
+
+  React.useEffect(() => {
+    if (reportData?.title) {
+      document.title = `${reportData.title} - ${APPLICATION_NAME}`;
+    } else {
+      document.title = `Untitled Report - ${APPLICATION_NAME}`;
+    }
+  }, [reportData?.title]);
 
   // Get selectedTabId from UI state and URL query params
   const selectedTabId = useSelectedTabId();
