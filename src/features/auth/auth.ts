3ccfb75abb1bed208aa2b474b33d89921213a137
Bradlee Krupa <bkrupa21@gmail.com>
2025-09-05 23:01:02 -0400

    Updated client id

--- src/features/auth/auth.ts
+++ src/features/auth/auth.ts
@@ -1,7 +1,7 @@
 // Compose redirect URI using PUBLIC_URL at build time
 export const REDIRECT_URI = `${window.location.origin}${process.env.PUBLIC_URL || ''}/#/oauth-redirect`;
 // Replace with your actual ESO Logs client ID
-export const CLIENT_ID = '9faa9dc1-0bea-4609-84e0-a4e02bbe0271';
+export const CLIENT_ID = '9fcf46ff-db45-4be9-92ea-9cc05fe45d49';
 export const PKCE_CODE_VERIFIER_KEY = 'eso_code_verifier';
 
 export const LOCAL_STORAGE_ACCESS_TOKEN_KEY = 'access_token';
