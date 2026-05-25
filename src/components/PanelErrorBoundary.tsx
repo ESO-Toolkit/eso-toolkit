7eb1524a3fbd59fb403a37689ec026a28b42e5c2
braydenpb72 <braydenpb72@gmail.com>
2026-05-25 00:31:10 -0700

    style: format all modified files with Prettier

--- src/components/PanelErrorBoundary.tsx
+++ src/components/PanelErrorBoundary.tsx
@@ -16,7 +16,13 @@ const PanelErrorFallback: React.FC<PanelErrorFallbackProps> = ({ panelName, onRe
       icon={<ErrorOutlined />}
       sx={{ width: '100%', maxWidth: 500 }}
       action={
-        <Button color="inherit" size="small" startIcon={<Refresh />} onClick={onRetry} sx={{ minHeight: 44, minWidth: 44 }}>
+        <Button
+          color="inherit"
+          size="small"
+          startIcon={<Refresh />}
+          onClick={onRetry}
+          sx={{ minHeight: 44, minWidth: 44 }}
+        >
           Retry
         </Button>
       }
