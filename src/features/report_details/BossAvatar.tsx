65ff6b5c9ce3740d683a45a9a68d1538d09e29bd
braydenpb72 <braydenpb72@gmail.com>
2026-05-24 19:28:25 -0700

    fix: add Opulent Ordeal zone data and fix asset filename typo
    - Add zone 1565 (Opulent Ordeal) to ZONE_SCALE_DATA and ZONE_NAMES
      sourced from elmseditor (mapId 2796, coordinates verified)
    - Rename tortued ranyu.png → tortured ranyu.png (fix typo in asset)
    - Update BossAvatar import path to match renamed file

--- src/features/report_details/BossAvatar.tsx
+++ src/features/report_details/BossAvatar.tsx
@@ -37,7 +37,7 @@ import hallOfFleshcraftAvatar from '@/assets/Ossein Cage/Boss Avatars/hall of fl
 import jynorahAndSkorkhifAvatar from '@/assets/Ossein Cage/Boss Avatars/jynorah and skorkhif.png';
 import overfiendKazpianAvatar from '@/assets/Ossein Cage/Boss Avatars/overfiend kazpian.png';
 import redWitchGednaRelvelAvatar from '@/assets/Ossein Cage/Boss Avatars/red witch gedna relvel.png';
-import torturedRanyuAvatar from '@/assets/Ossein Cage/Boss Avatars/tortued ranyu.png';
+import torturedRanyuAvatar from '@/assets/Ossein Cage/Boss Avatars/tortured ranyu.png';
 import ashTitanAvatar from '@/assets/Rockgrove/Boss Avatars/ash titan.png';
 import basksInSnakesAvatar from '@/assets/Rockgrove/Boss Avatars/basks-in-snakes.png';
 import flameHeraldBahseiAvatar from '@/assets/Rockgrove/Boss Avatars/flame-herald bahsei.png';
