73a1dd75e8e7e3d5818cb2c277e3337808eb7e39
Spike jones <BraydenPB72@gmail.com>
2026-06-14 21:06:26 -0700

    Revert "feat(calculator): all-class Ultimate Calculator tab on /calculator (#…" (#1234)
    This reverts commit 629d1b852d84bc1e738d2600d4cf9e183158b228.

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ const AppAuth = React.lazy(() =>
   import('./AppAuth').then((module) => ({ default: module.AppAuth })),
 );
 const Calculator = React.lazy(() =>
-  import('./components/CalculatorPage').then((module) => ({ default: module.CalculatorPage })),
+  import('./components/Calculator').then((module) => ({ default: module.Calculator })),
 );
 const TextEditor = React.lazy(() =>
   import('./components/TextEditor').then((module) => ({ default: module.TextEditor })),
